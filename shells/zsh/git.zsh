62a0c98114e1b9ada19f3bbce0961a6491535ba1
Huy Le <dacrazyazn@gmail.com>
2017-09-04 11:10:41 -0700

    trying to change tmuxinator colors to match vim. added pio to tmunxinator

--- shells/zsh/git.zsh
+++ shells/zsh/git.zsh
@@ -12,6 +12,7 @@ alias gmv='git mv'
 alias grm='git rm'
 alias grn='git-rename'
 alias glog="git l"
+alias gaca="git add . && git commit --amend"
 
 # alias git-amend='git commit --amend -C HEAD'
 alias git-undo='git reset --soft HEAD~1'
