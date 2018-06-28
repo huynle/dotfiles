4353d4ae0b0df8686dc12b824a1412eb207afeca
Huy Le <dacrazyazn@gmail.com>
2018-06-28 11:38:06 -0600

    updated tensorflow alias

--- shells/zsh/tf.zsh
+++ shells/zsh/tf.zsh
@@ -3,4 +3,4 @@ alias doc='nvidia-docker-compose'
 alias docl='doc logs -f --tail=100'
 
 
-alias tf="nvidia-docker run -it -p 8890:8888 tensorflow/tensorflow:latest-gpu"
+alias tf="docker run --runtime=nvidia -it -p 8890:8888 tensorflow/tensorflow:latest-gpu"
