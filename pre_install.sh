3de4510c25c75b52afd44e299db96176f8851649
Huy Le <dacrazyazn@gmail.com>
2018-12-30 02:24:23 +0000

    changed profile name from chromeos to crouton to differentiate between crouton and terminal

--- pre_install.sh
+++ pre_install.sh
@@ -1,4 +1,27 @@
-# setup locale
+# setup locales
+
+
+## DEBIAN
+# Determine OS platform
+UNAME=$(uname | tr "[:upper:]" "[:lower:]")
+# If Linux, try to determine specific distribution
+if [ "$UNAME" == "linux" ]; then
+    # If available, use LSB to identify distribution
+    if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
+        export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+    # Otherwise, use release info file
+    else
+        export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
+    fi
+fi
+# For everything else (or if above failed), just use generic identifier
+[ "$DISTRO" == "" ] && export DISTRO=$UNAME
+unset UNAME
+
+if [ "$DISTRO"  == *"Debian"* ]; then
+    apt-get install debconf
+fi
+
 locale-gen us_EN.UFT-8
 
 ## First thing first. Install git
