0de36b85a603f68a9afc1c56168bb24ae1b1142a
Huy Le <dacrazyazn@gmail.com>
2019-09-27 16:24:05 -0600

    clean up bash

--- shells/bash/env.bash
+++ shells/bash/env.bash
@@ -9,26 +9,26 @@ export PATH=$HOME/.local/bin:$PATH
 export EDITOR='vim'
 export GIT_EDITOR='vim'
 
-# Virtualenvs
-export WORKON_HOME=$HOME/.virtualenvs
-# source /usr/local/bin/virtualenvwrapper.sh
+# # Virtualenvs
+# export WORKON_HOME=$HOME/.virtualenvs
+# # source /usr/local/bin/virtualenvwrapper.sh
 
-# GoLang
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/GoProjects
-export PATH=$PATH:$(go env GOPATH)/bin
+# # GoLang
+# export PATH=$PATH:/usr/local/go/bin
+# export GOPATH=$HOME/GoProjects
+# export PATH=$PATH:$(go env GOPATH)/bin
 
-# Ninja build
-export PATH=/opt/ninja:$PATH
+# # Ninja build
+# export PATH=/opt/ninja:$PATH
 
-# export FZF_DEFAULT_COMMAND='ag -g ""'
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-export FZF_DEFAULT_OPTS='--height 40% --reverse --border --inline-info'
+# # export FZF_DEFAULT_COMMAND='ag -g ""'
+# export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+# export FZF_DEFAULT_OPTS='--height 40% --reverse --border --inline-info'
 
-# hizzle blog
-export HUGO_BIN="/home/hle/GoProjects/bin/hugo"
-export JOURNAL_DIR="$HOME/Documents/hizzle/content/private/journal"
-export BLOG_DIR="$HOME/Documents/hizzle"
+# # hizzle blog
+# export HUGO_BIN="/home/hle/GoProjects/bin/hugo"
+# export JOURNAL_DIR="$HOME/Documents/hizzle/content/private/journal"
+# export BLOG_DIR="$HOME/Documents/hizzle"
 
 BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
